aa9fd97ab55723e49d55968f1a156ee781c6ace5
Erik Almbratt <erik.almbratt@gmail.com>
2020-03-18 00:24:34 +0100

    Lös en jävla massa grejer med animationer. Buggar dock fortfarande när muspekaren är rakt över huvudet.

--- Player.gd
+++ Player.gd
@@ -5,7 +5,11 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 const FLOOR = Vector2(0, -1)
+const pixel_width = 23
+
 var state_machine
+var arm_state_machine
+var mouse_pos = Vector2()
 
 export var xspeed = 100
 export var yspeed = 40
@@ -15,7 +19,7 @@ var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	state_machine = $AnimatedSprite/AnimationTree.get("parameters/playback")
-	#state_machine.start("Idle")
+	arm_state_machine = $AnimatedSprite/Arm/AnimationTree.get("parameters/playback")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,11 +28,9 @@ func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = xspeed
 		state_machine.travel("Sword walk")
-		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -xspeed
 		state_machine.travel("Sword walk")
-		$AnimatedSprite.flip_h = true
 	else:
 		velocity.x = 0
 		
@@ -43,19 +45,35 @@ func _process(delta):
 		velocity.y = 0
 		
 	### Sheathe/unsheathe sword ###
-	if Input.is_action_just_pressed("sword"):
-		$AnimatedSprite/AnimationTree["parameters/conditions/Sword"] = true
-	else:
-		$AnimatedSprite/AnimationTree["parameters/conditions/Sword"] = false
+	#if Input.is_action_just_pressed("sword"):
+	#	$AnimatedSprite/AnimationTree["parameters/conditions/Sword"] = true
+	#else:d
+	#	$AnimatedSprite/AnimationTree["parameters/conditions/Sword"] = false
 		
+	### Aim sword ###
+	mouse_pos = get_viewport().get_mouse_position()
+	var angle = rad2deg($AnimatedSprite/Arm.global_position.angle_to_point(mouse_pos))
+	var angle_diff = angle - $AnimatedSprite/Arm.rotation_degrees - 180
+	$AnimatedSprite/Arm.rotation_degrees += angle_diff
+	
+	### H flip ###
+	if (angle > -90) and (angle < 90) and ($AnimatedSprite.flip_h == false):
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite/Arm.flip_v = true
+		$AnimatedSprite/Arm.position -= Vector2(pixel_width, 0)
+		$AnimatedSprite/Arm.offset += Vector2(0, pixel_width)
+	elif ((angle < -90) or (angle > 90)) and ($AnimatedSprite.flip_h == true):
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite/Arm.flip_v = false
+		$AnimatedSprite/Arm.position += Vector2(pixel_width, 0)
+		$AnimatedSprite/Arm.offset -= Vector2(0, pixel_width)
+	
+	### Attack ###	
+	if Input.is_action_pressed("attack"):
+		arm_state_machine.travel("Attack")
+	
 	### Idle animation ###
 	$AnimatedSprite/AnimationTree["parameters/conditions/Idle"] = (velocity.x == 0) and (velocity.y == 0)
 	
-	#if velocity.x == 0 && velocity.y == 0:
-		#if $AnimatedSprite.get_animation() == "Sword walk":
-		#	state_machine.travel("Idle sword")
-		#else:
-		#	state_machine.travel("Idle sword")
-		#state_machine.stop()
 		
 	velocity = move_and_slide(velocity, FLOOR)
